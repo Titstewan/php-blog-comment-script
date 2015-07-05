f49860ceb30f6bace52e19ded7b3abb7a7484106
Tìtstewan <palulutantsyip@gmail.com>
2015-07-05 23:15:34 +0200

    The script

---
+++ comment_area.php
@@ -0,0 +1,67 @@
+﻿<?php
+
+define ( 'MYSQL_HOST', 'localhost' );
+define ( 'MYSQL_USER', 'root' );
+define ( 'MYSQL_PASS', '' );
+define ( 'MYSQL_DATA', 'commentfunction' );
+
+$db_link = @mysql_connect (MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
+
+if ( ! $db_link ) {
+    die('Failed to etablish connection to the database. Check it out later!');
+  }
+
+$db_sel = mysql_select_db( MYSQL_DATA )
+  or die('Unable to select database.');
+
+$sql = " SELECT * FROM comments ORDER BY date ";
+
+$db_erg = mysql_query( $sql );
+  if ( ! $db_erg ) {
+  die('Invalid query: ' . mysql_error()); }
+
+
+// testing here
+
+$entries = mysql_num_rows($db_erg);
+  echo "<p>Amount of comments: $entries </p>";
+
+while ($data = mysql_fetch_array( $db_erg, MYSQL_ASSOC))
+{
+    // Aushabe der Daten
+    echo "ID: ";
+    echo $data['id'];
+    echo "<br />";
+ 
+    echo "Name: ";
+    echo $data['name'];
+    echo "<br />";
+ 
+    echo "Email: ";
+    echo $data['email'];
+    echo "<br />";
+ 
+    echo "URL: ";
+    echo $data['url'];
+    echo "<br />";
+ 
+    echo "Date: ";
+    echo $data['date'];
+    echo "<br />";
+ 
+    echo "Comment: ";
+    echo $data['comment'];
+    echo "<br />";
+}
+
+mysql_free_result( $db_erg );
+
+
+
+
+
+
+
+
+
+?>
---
+++ new_comment.php
@@ -0,0 +1,53 @@
+﻿<form name="" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="text/html">
+  <p>Name:<br />
+    <input type="text" name="name" value="" size="50" maxlength="150" />
+  </p>
+  <p>Email:<br />
+    <input type="text" name="email" value="" size="50" maxlength="150" />
+  </p>
+  <p>Homepage:<br />
+    <input type="text" name="url" value="" size="50" maxlength="150" />
+  </p>
+  <p>Comment:<br />
+    <textarea name="comment_field" rows="10" cols="50"></textarea>
+  </p>
+  <input type="Submit" name="" value="save" />
+</form>
+
+<?php
+
+if ( !isset($_POST['comment_field']) != "" ) {
+  echo "<h2>Save comment</h2>";
+
+  $sql = " INSERT INTO comments ";
+  $sql .= " SET ";
+  $sql .= " name ='". !isset($_POST['name']) ."', ";
+  $sql .= " email ='". !isset($_POST['email']) ."', ";
+  $sql .= " url ='". !isset($_POST['url']) ."', ";
+  $sql .= " date ='". date("Y-m-d H:i:s") ."', ";
+  $sql .= " comment ='". !isset($_POST['comment_field']) ."' ";
+
+define ( 'MYSQL_HOST', 'localhost' );
+define ( 'MYSQL_USER', 'root' );
+define ( 'MYSQL_PASS', '' );
+define ( 'MYSQL_DATA', 'commentfunction' );
+
+$db_link = @mysql_connect (MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
+
+  if ( ! $db_link ) {
+  die('Failed to etablish connection to the database. Check it out later!');
+  }
+
+$db_sel = mysql_select_db ( MYSQL_DATA )
+  or die('Unable to select database.');
+
+$db_erg = mysql_query( $sql );
+  if ( ! $db_erg ) {
+  die('Invalid query: ' . mysql_error());
+  }
+  }
+
+  echo '<p><a href="comment_area.php">See comment</a></p>';
+  exit;
+
+?>
